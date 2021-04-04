d8ab0caa26658cff758248406be84e92392c5036
wathi <hoiinwat@gmail.com>
2021-04-04 12:11:47 +0900

    wat_20210404

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,8 +37,11 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <!-- {{ config('app.name', 'Laravel') }} -->
-                    タスク管理アプリ
+                    {{-- <!-- {{ config('app.name', 'Laravel') }} --> --}}
+                    <span class="text-dark">
+                        <i class="far fa-check-circle"></i>
+                        タスク管理
+                    </span>                    
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
