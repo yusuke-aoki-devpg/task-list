4ec4716b96e54c633916373b10523e15f880074e
ymktmk <ymktmk.co@gmail.com>
2021-04-05 18:47:58 +0900

    4/5

--- database/migrations/2021_03_08_012316_create_todos_table.php
+++ database/migrations/2021_03_08_012316_create_todos_table.php
@@ -15,7 +15,7 @@ class CreateTodosTable extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->text('todo')->nullable(false);;
+            $table->text('todo')->nullable(false);
             $table->dateTime('deadline')->nullable(false);
             $table->timestamps();
             $table->bigInteger('user_id')->unsigned();
